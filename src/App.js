07dff310efa461e66848dbb801db306e439f16e4
Eliel Mamousette <eliel@artistory.net>
2020-11-28 04:47:46 -0500

    fix env name

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            {process.env.PGHOST}
+            {process.env.REACT_APP_PGHOST}
           </a>
         </header>
       </div>
