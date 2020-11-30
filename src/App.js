2f32d09d67c0d749600a1881242cc95b5e90ba36
Eliel Mamousette <eliel@artistory.net>
2020-11-30 08:34:57 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from '@aws-amplify/core';
 import aws_exports from './aws-exports';
-const { Client, Pool } = require('pg').native
 
 Amplify.configure(aws_exports);
 
@@ -12,44 +11,14 @@ class App extends Component {
     // Add your code here
   
   render() {
-    const pg = new Client({
-      user: 'postgres',
-      host: 'primary.cqfn2pbb0weo.us-east-1.rds.amazonaws.com',
-      database: 'doceoarts',
-      password: 'ArtEducates#Children!',
-      port: 5432
-    })
-  
-    pg.connect()
-    
-    pg.query(`select * from artist_engagement ae 
-    join artist a on a.id = ae.artist_id 
-    join engagement e on e.id = ae.engagement_id`,
-    (err, rows) => {
-      if (err) throw err;
-      let res = rows;
-      console.log( res );
-      pg.end();
-      });
-    
 
-  
-    console.log(process.env.REACT_APP_PGHOST );
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Not an Authorized User
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {process.env.REACT_APP_PGHOST}
-          </a>
         </header>
       </div>
     );
